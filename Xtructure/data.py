7fcd46d5c7553ebe16a9d834773774852aede192
tinker495 <wjdrbtjr495@gmail.com>
2025-05-08 15:19:09 +0900

    decorator

--- Xtructure/data.py
+++ Xtructure/data.py
@@ -1,9 +1,10 @@
 import chex
 import jax
 import jax.numpy as jnp
+import typing # Added for Protocol and TYPE_CHECKING
 
 from collections import namedtuple
-from typing import TypeVar, Type, Dict, Any
+from typing import TypeVar, Type, Dict, Any, Tuple as TypingTuple, Protocol # Added Protocol, TypingTuple
 from enum import Enum
 from tabulate import tabulate
 
@@ -11,6 +12,57 @@ MAX_PRINT_BATCH_SIZE = 4
 SHOW_BATCH_SIZE = 2
 
 T = TypeVar("T")
+
+
+# Protocol defining the interface added by @xtructure_data
+class Xtructurable(Protocol[T]):
+    # Fields from the original class that chex.dataclass would process
+    # These are implicitly part of T. For the protocol to be complete,
+    # it assumes T will have __annotations__.
+    __annotations__: Dict[str, Any]
+    # __dict__ is used by the __getitem__ implementation
+    __dict__: Dict[str, Any]
+
+
+    # Methods and properties added by add_shape_dtype_getitem_len
+    @property
+    def shape(self) -> Any: # Actual type is a dynamically generated namedtuple
+        ...
+    @property
+    def dtype(self) -> Any: # Actual type is a dynamically generated namedtuple
+        ...
+    def __getitem__(self: T, index: Any) -> T:
+        ...
+    def __len__(self) -> int:
+        ...
+
+    # Methods and properties added by add_default_parser
+    # Assumes the class T has a 'default' classmethod as per the decorator's assertion
+    @classmethod
+    def default(cls: Type[T]) -> T:
+        ...
+    @property
+    def default_shape(self) -> Any: # Derived from self.default().shape
+        ...
+    @property
+    def structured_type(self) -> 'StructuredType': # Forward reference for StructuredType
+        ...
+    @property
+    def batch_shape(self) -> TypingTuple[int, ...]:
+        ...
+    def reshape(self: T, new_shape: TypingTuple[int, ...]) -> T:
+        ...
+    def flatten(self: T) -> T:
+        ...
+    @classmethod
+    def random(cls: Type[T], shape: TypingTuple[int, ...] = ..., key: Any = ...) -> T: # Ellipsis for default value
+        ...
+
+    # Methods and properties added by add_string_parser
+    def __str__(self) -> str: # The actual implementation takes **kwargs, but signature can be simpler for Protocol
+        ...
+    def str(self) -> str: # Alias for __str__
+        ...
 
 
 def isnamedtupleinstance(x):
@@ -39,7 +91,7 @@ def get_leaf_elements(tree: chex.Array):
     else:
         yield tree  # Yield the leaf element
 
-def xtructure_data(cls):
+def xtructure_data(cls: Type[T]) -> Type[Xtructurable[T]]:
     """
     Decorator that creates a dataclass for HeapValue with additional functionality.
     Adds shape, dtype, getitem, and len properties to the class.
