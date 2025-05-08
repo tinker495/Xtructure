a71e729f583ce26e9338114e1da8c711928dc367
tinker495 <wjdrbtjr495@gmail.com>
2025-05-08 15:59:47 +0900

    update

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Framework :: JAX",
     ],
     python_requires=">=3.8",
 ) 
