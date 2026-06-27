7827a63553e876233cff53c5abccef187917fa46
tinker495 <wjdrbtjr495@gmail.com>
2026-06-27 18:17:16 +0900

    refactor: delete unused pallas loop merge

--- xtructure/bgpq/merge_split/__init__.py
+++ xtructure/bgpq/merge_split/__init__.py
@@ -3,13 +3,11 @@ from __future__ import annotations
 from xtructure._lazy_imports import lazy_dir, load_lazy_export
 
 __all__ = [
-    "merge_arrays_indices_loop",
     "merge_arrays_parallel",
     "merge_sort_split_idx",
 ]
 
 _EXPORTS = {
-    "merge_arrays_indices_loop": (".loop", "merge_arrays_indices_loop"),
     "merge_arrays_parallel": (".parallel", "merge_arrays_parallel"),
     "merge_sort_split_idx": (".split", "merge_sort_split_idx"),
 }
